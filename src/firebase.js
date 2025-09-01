eabfa90a2be61ea8a0cd3b0044865d0718d2fee7
Aditya Ramakrishnan <aditya.ramki1@gmail.com>
2025-08-31 22:28:23 -0400

    ignore

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbNfEmxvQUuw_kcv1kGtlsvE_RTzFS0yk",
-  authDomain: "spartan-swap.firebaseapp.com",
-  projectId: "spartan-swap",
-  storageBucket: "spartan-swap.firebasestorage.app",
-  messagingSenderId: "413276883867",
-  appId: "1:413276883867:web:62bbeb376cb2c86526ef3e",
-  measurementId: "G-D3N8NYFR6K"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
